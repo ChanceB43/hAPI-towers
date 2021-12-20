0fbf840a63576ab083dc86113aa197f681fb3528
kinglogan <glogan80@gmail.com>
2021-12-20 12:20:28 -0600

    mongo connected

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/hapitowers', {
+     useNewUrlParser: true, 
+     useUnifiedTopology: true 
+})
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!")
+        console.log(err)
+    });
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/towers", (req, res) => {
+    res.send("hAPI TOWERS!")
+})
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000")
+})
